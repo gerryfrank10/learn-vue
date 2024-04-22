85ea7438e26bbd2de5c8dfcd65513a22683ffe95
gerald francis <gerryfrank720@outlook.com>
2024-04-22 13:59:41 +0300

    Adding form working now

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TodoItem from "@/components/TodoItem.vue";
+import TodoForm from "@/components/TodoForm.vue";
 import {ref} from "vue";
 import uniqueId from "lodash.uniqueid";
 
@@ -8,10 +9,18 @@ const toDoItems = ref([
   {id: uniqueId("todo-"), label: "Having Fun with MAUI", done: true},
   {id: uniqueId("todo-"), label: "Finish CTF Project", done: false},
 ]);
+
+function addToDo(todoLabel) {
+  toDoItems.value.push({id: uniqueId("todo-"), label: todoLabel, done: false});
+  console.log("To-do added", todoLabel);
+}
+
 </script>
 
 <template>
   <div>
+    <h1>My Todo List</h1>
+    <todo-form @todo-added="addToDo"></todo-form>
     <div v-for="todo in toDoItems" :key="todo.id">
       <todo-item :id="todo.id" :todo="todo" :label="todo.label" :done="todo.done"></todo-item>
     </div>
--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,9 +1,34 @@
 <script setup>
 
+import {getCurrentInstance, ref} from "vue";
+
+const instance = getCurrentInstance();
+const label = ref('');
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if (label.value === '') {
+    return
+  }
+  instance.emit('todo-added', label.value)
+}
+
 </script>
 
 <template>
-
+  <form @submit.prevent="handleSubmit">
+    <label for="new-todo-input">What needs to be done? </label>
+    <input
+        type="text"
+        id="new-todo-input"
+        placeholder="What needs to be done."
+        autocomplete="off"
+        v-model="label"
+    />
+    <button type="submit">Add</button>
+    <p>Message : {{label}}</p>
+  </form>
 </template>
 
 <style scoped>
